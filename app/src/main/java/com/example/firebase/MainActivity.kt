2b46ade8c84e8c9d7d11b0c1b88414f2c688ecc4
David A. King <k2_daveking@hotmail.com>
2023-02-26 18:15:52 -0500

    Lesson #158 Completed working code from end of 158.

--- app/src/main/java/com/example/firebase/MainActivity.kt
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -9,45 +9,10 @@ import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var editTextName : EditText
-    lateinit var buttonSend : Button
-    lateinit var textViewName : TextView
-
-    val dataBase : FirebaseDatabase = FirebaseDatabase.getInstance()
-    val reference : DatabaseReference = dataBase.reference.child("users")
-    val reference2 : DatabaseReference = dataBase.reference
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        editTextName =  findViewById(R.id.editTextName)
-        buttonSend = findViewById(R.id.buttonSend)
-        textViewName = findViewById(R.id.textViewName)
-
-        reference2.addValueEventListener(object : ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-
-            val realName : String = snapshot.child("users").child("name").value as String
-
-                textViewName.text = realName
-            } // end onDataChanged fun
-
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            } // end onCancelled func
-        }) // end reference2 block
-
-
-        buttonSend.setOnClickListener {
-
-            val userName = editTextName.text.toString()
-
-            reference.child("userName").setValue(userName)
-
-
-        }
 
     }
 }
