8aacb896db3f27c650a1bb8dbb76f861f5f08b27
David A. King <k2_daveking@hotmail.com>
2023-03-01 22:26:17 -0500

    Lesson #161 Completed working code from end of Section 26 Lesson 161 Retrieving data from the Realtime Database

--- app/src/main/java/com/example/firebase/MainActivity.kt
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -13,6 +13,13 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var mainBinding : ActivityMainBinding
 
+    // Create instance of Firebase Database
+    val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+    // get reference to our data object child  we want
+    val myReference : DatabaseReference = database.reference.child("MyUsers")
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // setup binding to view objects
@@ -25,7 +32,41 @@ class MainActivity : AppCompatActivity() {
             // floating action button clicked, open addUserActivity
         val intent = Intent(this, AddUserActivity::class.java)
         startActivity(intent)
-        }
+        } // end onClick listener for FAButton
+
+        // call data retrieval function
+        retrieveDataFromDatabase()
+
+    } // end onCreate function
+
+    fun retrieveDataFromDatabase(){
+
+        myReference.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                // retrieve the data, loop through database child branch
+                for (eachuser in snapshot.children){
+
+                    val user = eachuser.getValue(Users::class.java)
+
+                    // print the data to the logcat window if not null
+                    if (user != null){
+                        println("userId: ${user.userId}")
+                        println("userName: ${user.userName}")
+                        println("userAge: ${user.userAge}")
+                        println("userEmail: ${user.userEmail}")
+                        println("*******************************")
+                    } // end IF
+
+                } // end FOR action for each user data
+
+            } // end onDataChange function actions
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
 
     }
-}
+
+} // end class MainActivity
