81328624f4ac9eb1ad66b447ae09ebb7eea06119
David A. King <k2_daveking@hotmail.com>
2023-03-10 22:12:27 -0500

    Lesson #165 Completed working code from end of Section 26, Lesson 165 User Registration App - Updating Data Part 2

--- app/src/main/java/com/example/firebase/UpdateUserActivity.kt
+++ app/src/main/java/com/example/firebase/UpdateUserActivity.kt
@@ -2,11 +2,17 @@ package com.example.firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.firebase.databinding.ActivityUpdateUserBinding
+import com.google.firebase.database.FirebaseDatabase
 
 class UpdateUserActivity : AppCompatActivity() {
 
     lateinit var updateUserBinding: ActivityUpdateUserBinding
+
+    // create instance of database in global area
+    val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+    val myReference = database.reference.child("MyUsers")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +26,13 @@ class UpdateUserActivity : AppCompatActivity() {
         // call the function to get the data and update the screen
         getAndSetData()
 
+        // set a click listener for the update button
+        updateUserBinding.buttonUpdateUser.setOnClickListener {
+
+            updateData()
+
+        }
+
     } // end onCreate actions
 
     fun getAndSetData(){
@@ -32,6 +45,40 @@ class UpdateUserActivity : AppCompatActivity() {
         updateUserBinding.editTextUpdateName.setText(name)
         updateUserBinding.editTextUpdateAge.setText(age)
         updateUserBinding.editTextUpdateEmail.setText(email)
+
+    }
+
+    fun updateData(){
+
+        // get the data from the screen entry fields
+        val updatedName = updateUserBinding.editTextUpdateName.text.toString()
+        val updatedAge = updateUserBinding.editTextUpdateAge.text.toString().toInt()
+        val updatedEmail = updateUserBinding.editTextUpdateEmail.text.toString()
+        // record ID was passed from intent data passed to activity
+        val userId = intent.getStringExtra("id").toString()
+
+        val userMap = mutableMapOf<String, Any>()
+        userMap["userId"] = userId
+        userMap["userName"] = updatedName
+        userMap["userAge"] = updatedAge
+        userMap["userEmail"] = updatedEmail
+
+        myReference.child(userId).updateChildren(userMap)
+            .addOnCompleteListener { task ->
+
+            if (task.isSuccessful){
+
+                Toast.makeText(applicationContext, "The user has been " +
+                        "updated", Toast.LENGTH_LONG).show()
+
+                // close the activity , open mainActivity
+                finish()
+
+            }
+
+
+        }
+
 
 
 
