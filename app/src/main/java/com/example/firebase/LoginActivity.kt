697aa617674ff267ad9f903658ae3fc1639b278b
David A. King <k2_daveking@hotmail.com>
2023-03-18 16:11:13 -0400

    Lesson #171 Completed working code from Section 27, Lesson 171 Email and Password Authentication - Part 3

--- app/src/main/java/com/example/firebase/LoginActivity.kt
+++ app/src/main/java/com/example/firebase/LoginActivity.kt
@@ -3,11 +3,15 @@ package com.example.firebase
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.firebase.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var loginBinding: ActivityLoginBinding
+
+    var auth : FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +23,11 @@ class LoginActivity : AppCompatActivity() {
 
         loginBinding.buttonSignin.setOnClickListener {
 
+            val userEmail = loginBinding.editTextEmailSignin.text.toString()
+            val userPassword = loginBinding.editTextPasswordSignin.text.toString()
+            // call sign in function
+            signinWithFirebase(userEmail, userPassword)
+
         }
 
         loginBinding.buttonSignup.setOnClickListener {
@@ -27,5 +36,30 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
 
         }
+    }
+
+    fun signinWithFirebase(userEmail : String, userPassword : String){
+
+        auth.signInWithEmailAndPassword(userEmail, userPassword)
+            .addOnCompleteListener {task ->
+            if(task.isSuccessful){
+                // show taost message
+                Toast.makeText(applicationContext,"Login is successful",
+                    Toast.LENGTH_LONG).show()
+                // start MainActivity
+                 val intent = Intent(this@LoginActivity, MainActivity::class
+                     .java)
+                startActivity(intent)
+                // close this activity
+                finish()
+
+            }else {
+                // show message why failed
+                Toast.makeText(applicationContext,task.exception.toString(),
+                    Toast.LENGTH_LONG).show()
+
+            }
+        }
+
     }
 }
