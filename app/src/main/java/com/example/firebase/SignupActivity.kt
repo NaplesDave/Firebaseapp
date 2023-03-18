4ade1a8c4373094a26de10a42e45db3b4d99222e
David A. King <k2_daveking@hotmail.com>
2023-03-17 22:52:18 -0400

    Lesson #170 Completed working code from Section 27, Lesson 170 Email and Password Authentication - Part 2

--- app/src/main/java/com/example/firebase/SignupActivity.kt
+++ app/src/main/java/com/example/firebase/SignupActivity.kt
@@ -2,11 +2,16 @@ package com.example.firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.firebase.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class SignupActivity : AppCompatActivity() {
 
     lateinit var signupBinding: ActivitySignupBinding
+
+    val auth : FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +23,32 @@ class SignupActivity : AppCompatActivity() {
 
         signupBinding.buttonSignupUser.setOnClickListener {
 
+            val userEmail = signupBinding.editTextEmailSignup.text.toString()
+            val userPassword = signupBinding.editTextPasswordSignup.text.toString()
 
+            signupWithFirebase(userEmail, userPassword)
         }
+    }  // end onCreate function
+
+    fun signupWithFirebase(userEmail : String, userPassword : String){
+        //
+        auth.createUserWithEmailAndPassword(userEmail, userPassword)
+            .addOnCompleteListener { task ->
+
+                if(task.isSuccessful){
+                    Toast.makeText(applicationContext, "Your account has been" +
+                            " created", Toast.LENGTH_LONG).show()
+                    finish()
+
+                }else {
+                    // show toast why it failed
+                    Toast.makeText(applicationContext, task.exception?.toString(), Toast
+                        .LENGTH_LONG).show()
+
+                }
+        }
+
+
     }
+
 }
