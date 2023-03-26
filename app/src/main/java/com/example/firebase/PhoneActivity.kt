6c41027a48a8fbad65b2a9a19cdb2a22ebd75189
David A. King <k2_daveking@hotmail.com>
2023-03-25 22:40:40 -0400

    Lesson #175 Completed working code from Section 27, Lesson 175 Authentication with Phone Part - 2

--- app/src/main/java/com/example/firebase/PhoneActivity.kt
+++ app/src/main/java/com/example/firebase/PhoneActivity.kt
@@ -1,13 +1,26 @@
 package com.example.firebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.firebase.databinding.ActivityPhoneBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 class PhoneActivity : AppCompatActivity() {
 
     lateinit var phoneBinding : ActivityPhoneBinding
 
+    val auth : FirebaseAuth = FirebaseAuth.getInstance()
+
+    lateinit var mCallbacks : PhoneAuthProvider.OnVerificationStateChangedCallbacks
+
+    var verificationCode = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,13 +31,77 @@ class PhoneActivity : AppCompatActivity() {
         setContentView(view)
 
         phoneBinding.buttonSendSMSCode.setOnClickListener {
+            // get users phone number
+            val userPhoneNumber =
+                phoneBinding.editTextPhoneNumber.text.toString()
 
+            val options = PhoneAuthOptions.newBuilder(auth)
+                .setPhoneNumber(userPhoneNumber)
+                .setTimeout(60L, TimeUnit.SECONDS)
+                .setActivity(this@PhoneActivity)
+                .setCallbacks(mCallbacks)
+                .build()
 
+            PhoneAuthProvider.verifyPhoneNumber(options)
         }
 
         phoneBinding.buttonVerify.setOnClickListener {
 
+            signinWithSMSCode()
 
         }
+
+        mCallbacks = object : PhoneAuthProvider
+        .OnVerificationStateChangedCallbacks(){
+            override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onVerificationFailed(p0: FirebaseException) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCodeSent(
+                p0: String,
+                p1: PhoneAuthProvider.ForceResendingToken
+            ) {
+                super.onCodeSent(p0, p1)
+
+                verificationCode = p0
+
+            }
+
+        }
+
     }
-}
+
+    fun signinWithSMSCode(){
+        // get the user entered code from the screen
+        val userEnterCode = phoneBinding.editTextVerificationCode.text.toString()
+
+        // check the user code with the sent code with Firebase
+        val credential = PhoneAuthProvider.getCredential(verificationCode, userEnterCode)
+
+        signinWithPhoneAuthCredential(credential)
+    }
+
+    fun signinWithPhoneAuthCredential(credential: PhoneAuthCredential){
+
+        auth.signInWithCredential(credential).addOnCompleteListener { task ->
+
+            if (task.isSuccessful){
+                // open the MainActivity
+                val intent = Intent(this@PhoneActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            }else {
+                Toast.makeText(applicationContext, "The code you entered is " +
+                        "incorrect", Toast.LENGTH_LONG).show()
+
+            }
+
+        }
+
+    }
+} // end PhoneActivity class
